d86af39076d97e5f68e0484a4a20581b3fa0d743
Emily Valdez <emily.milan.valdez@gmail.com>
2023-10-10 12:19:15 -0500

    solution

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  
+  function handleClick() {
+    setInCart(!inCart);
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className={inCart ? "remove" : "add"}>{inCart ? "Remove from cart" : "Add to cart"}
+      </button>
     </li>
   );
 }
 
 export default Item;
+
+
+//  const appClass = darkMode ? "App dark" : "App light"
